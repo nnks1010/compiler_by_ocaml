0df44310e8a9c74f453cb196e4ee3851edc9a08b
nnks1010 <ntomoya1010@gmail.com>
2018-08-18 17:22:39 +0900

    chapter2: Exercises 1

--- chapter2/interp.ml
+++ chapter2/interp.ml
@@ -1,16 +1,17 @@
 (* syntax tree *)
-type id  = string ;;
-type op  = Plus | Minus | Times | Div ;;
+type id  = string
+type op  = Plus | Minus | Times | Div
 
-type stm = Stmts  of stm * stm
-         | Assign of id  * exp
-         | Print  of exp
-and exp  = ID     of id
-         | Num    of int
-         | Plus   of exp * exp
-         | Minus  of exp * exp
-         | Times  of exp * exp
-         | Div    of exp * exp ;;
+type stm = Stmts   of stm * stm
+         | Assign  of id  * exp
+         | Print   of exp
+and exp  = Id      of id
+         | Num     of int
+         | StmtExp of stm * exp
+         | Plus    of exp * exp
+         | Minus   of exp * exp
+         | Times   of exp * exp
+         | Div     of exp * exp
 
 (* environment *)
 exception No_such_symbol ;;
@@ -28,8 +29,10 @@ let rec trans_stmt ast env =
                                         (print_int vl; print_string "\n"; env)
 and trans_exp ast env =
     match ast with
-        ID  v -> env v
+        Id  v -> env v
       | Num n -> n
+      | StmtExp (s, e) -> let env' = trans_stmt s env in
+                                       trans_exp e env'
       | Plus (e1, e2)  -> let vl1 = trans_exp e1 env in
                             let vl2 = trans_exp e2 env in
                               vl1 + vl2
@@ -43,8 +46,17 @@ and trans_exp ast env =
                             let vl2 = trans_exp e2 env in
                               vl1 / vl2;;
 
+(* x = 1 + 2 * 3; y = x / 4; print (y) //=> 1 *)
 let prog = Stmts (Assign ("x", Plus (Num 1, Times (Num 2, Num 3))),
-             Stmts (Assign ("y", Div (ID "x", Num 4)), Print (ID "y")))
+             Stmts (Assign ("y", Div (Id "x", Num 4)), Print (Id "y"))) ;;
+
+(* y = (x = 1 + 2, x); print (y) //=> 3 *)
+let prog1 = Stmts (Assign("y",
+              StmtExp (Assign ("x", Plus (Num 1, Num 2)), Id "x")), Print (Id "y")) ;;
+
+(* y = (x = 1 + 2, x); print (x) //=> Exception: No_such_symbol *)
+let prog2 = Stmts (Assign("y",
+              StmtExp (Assign ("x", Plus (Num 1, Num 2)), Id "x")), Print (Id "x")) ;;
 
 let interp ast = trans_stmt ast e0
 
