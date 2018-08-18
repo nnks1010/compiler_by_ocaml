e84e823f84cdf99d0b39cf8ba9151a3d6ae22dcd
nnks1010 <ntomoya1010@gmail.com>
2018-08-18 18:56:32 +0900

    chapter2: Exercises 2

--- chapter2/interp.ml
+++ chapter2/interp.ml
@@ -14,37 +14,37 @@ and exp  = Id      of id
          | Div     of exp * exp
 
 (* environment *)
-exception No_such_symbol ;;
-let e0 = fun _ -> raise No_such_symbol ;;
+exception No_such_symbol
+let e0 = fun _ -> raise No_such_symbol
 let update var vl env = fun v -> if v = var then vl else env v
 
 (* semantic analysis *)
 let rec trans_stmt ast env =
     match ast with
-        Stmts (s1, s2)  -> let env' = trans_stmt s1 env in
-                                        trans_stmt s2 env'
-      | Assign (var, e) -> let vl   = trans_exp e env in
-                                        update var vl env
-      | Print e         -> let vl   = trans_exp e env in
-                                        (print_int vl; print_string "\n"; env)
+        Stmts (s1, s2)  -> let env'     = trans_stmt s1 env in
+                                            trans_stmt s2 env'
+      | Assign (var, e) -> let vl, env' = trans_exp e env in
+                                            update var vl env'
+      | Print e         -> let vl, env' = trans_exp e env in
+                                            (print_int vl; print_string "\n"; env')
 and trans_exp ast env =
     match ast with
-        Id  v -> env v
-      | Num n -> n
+        Id  v -> (env v, env)
+      | Num n -> (n, env)
       | StmtExp (s, e) -> let env' = trans_stmt s env in
                                        trans_exp e env'
-      | Plus (e1, e2)  -> let vl1 = trans_exp e1 env in
-                            let vl2 = trans_exp e2 env in
-                              vl1 + vl2
-      | Minus (e1, e2) -> let vl1 = trans_exp e1 env in
-                            let vl2 = trans_exp e2 env in
-                              vl1 - vl2
-      | Times (e1, e2) -> let vl1 = trans_exp e1 env in
-                            let vl2 = trans_exp e2 env in
-                              vl1 * vl2
-      | Div (e1, e2)   -> let vl1 = trans_exp e1 env in
-                            let vl2 = trans_exp e2 env in
-                              vl1 / vl2;;
+      | Plus (e1, e2)  -> let vl1, env1 = trans_exp e1 env in
+                            let vl2, env2 = trans_exp e2 env1 in
+                              (vl1 + vl2, env2)
+      | Minus (e1, e2) -> let vl1, env1 = trans_exp e1 env in
+                            let vl2, env2 = trans_exp e2 env1 in
+                              (vl1 - vl2, env2)
+      | Times (e1, e2) -> let vl1, env1 = trans_exp e1 env in
+                            let vl2, env2 = trans_exp e2 env1 in
+                              (vl1 * vl2, env2)
+      | Div (e1, e2)   -> let vl1, env1 = trans_exp e1 env1 in
+                            let vl2, env2 = trans_exp e2 env2 in
+                              (vl1 / vl2, env2)
 
 (* x = 1 + 2 * 3; y = x / 4; print (y) //=> 1 *)
 let prog = Stmts (Assign ("x", Plus (Num 1, Times (Num 2, Num 3))),
@@ -54,7 +54,7 @@ let prog = Stmts (Assign ("x", Plus (Num 1, Times (Num 2, Num 3))),
 let prog1 = Stmts (Assign("y",
               StmtExp (Assign ("x", Plus (Num 1, Num 2)), Id "x")), Print (Id "y")) ;;
 
-(* y = (x = 1 + 2, x); print (x) //=> Exception: No_such_symbol *)
+(* y = (x = 1 + 2, x); print (x) //=> 3 *)
 let prog2 = Stmts (Assign("y",
               StmtExp (Assign ("x", Plus (Num 1, Num 2)), Id "x")), Print (Id "x")) ;;
 
